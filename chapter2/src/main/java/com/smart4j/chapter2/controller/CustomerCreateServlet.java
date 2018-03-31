8384eeb8d0b8c0099147a83250d3d61ac149a257
dongshuiquan <1160161078@qq.com>
2018-03-31 13:41:02 +0800

    smart4j

---
+++ chapter2/src/main/java/com/smart4j/chapter2/controller/CustomerCreateServlet.java
@@ -0,0 +1,24 @@
+package com.smart4j.chapter2.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by DELL on 2018/3/31.
+ */
+@WebServlet("/customer_create")
+public class CustomerCreateServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // TODO
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // TODO
+    }
+}
---
+++ chapter2/src/test/java/com/smart4j/chapter2/test/CustomerServiceTest.java
@@ -0,0 +1,68 @@
+package com.smart4j.chapter2.test;
+
+import com.smart4j.chapter2.model.Customer;
+import com.smart4j.chapter2.service.CustomerService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by DELL on 2018/3/31.
+ */
+public class CustomerServiceTest {
+
+    private final CustomerService customerService;
+
+    public CustomerServiceTest() {
+        customerService = new CustomerService();
+    }
+
+    @Before
+    public void init() {
+
+    }
+
+    @Test
+    public void getCustomListTest() {
+        List<Customer> customerList = customerService.getCustomerList();
+        System.out.println(customerList);
+        Assert.assertEquals(2, customerList.size());
+    }
+
+    @Test
+    public void getCustomerTest() {
+        long id = 1;
+        Customer customer = customerService.getCustomer(id);
+        Assert.assertNotNull(customer);
+    }
+
+    @Test
+    public void createCustomerTest() {
+        Map<String, Object> fieldMap = new HashMap<String, Object>();
+        fieldMap.put("name", "customer100");
+        fieldMap.put("contact", "John");
+        fieldMap.put("telephone", "13512345678");
+        boolean result = customerService.createCustomer(fieldMap);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void updateCustomerTest() {
+        long id = 1;
+        Map<String, Object> fieldMap = new HashMap<String, Object>();
+        fieldMap.put("contact", "Eric");
+        boolean result = customerService.updateCustomer(id, fieldMap);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void deleteCustomerList() {
+        long id = 1;
+        boolean result = customerService.deleteCustomer(id);
+        Assert.assertTrue(result);
+    }
+}
